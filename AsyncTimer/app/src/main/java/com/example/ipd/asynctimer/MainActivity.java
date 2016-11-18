4ca0c8dd79cbc5311b65dce6b86c11f181237855
Ali Torabi <alitorabi1@gmail.com>
2016-11-18 13:47:16 -0500

    18 Nov 2016

--- AsyncTimer/app/src/main/java/com/example/ipd/asynctimer/MainActivity.java
+++ AsyncTimer/app/src/main/java/com/example/ipd/asynctimer/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.ipd.asynctimer;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+        import android.os.AsyncTask;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
-import java.util.Timer;
-import java.util.TimerTask;
+        import java.util.Timer;
+        import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,49 +23,51 @@ public class MainActivity extends AppCompatActivity {
         txt = (TextView) findViewById(R.id.textView);
     }
 
+    UpdateTask updateTask = new UpdateTask();
+
     public void startApp(View view) {
         // detect the view that was "clicked"
-        switch (view.getId()) {
-            case R.id.button:
-                new UpdateTask().execute();
-                break;
+        if (updateTask != null && updateTask.getStatus() == AsyncTask.Status.RUNNING){
+            if (updateTask.isCancelled()) {
+                updateTask = new UpdateTask();
+                updateTask.execute();
+            }
+        } else {
+            updateTask.execute();
         }
     }
 
-    private class UpdateTask extends AsyncTask<Void, Integer, Void> {
+    public void stopApp(View view) {
+        // detect the view that was "clicked"
+        updateTask.cancel(true);
+    }
+
+    class UpdateTask extends AsyncTask<Void, Integer, Void> {
 
         protected void onProgressUpdate(Integer... i) {
-            // code to update the user interface goes here
+            // code to update the user interface
             super.onProgressUpdate(i);
             txt.setText(i[0].toString());
         }
 
-
         @Override
         protected Void doInBackground(Void... params) {
             int i = 0;
-            while (!isCancelled()) {
-                // you will need to call the publishProgress method somewhere here
+            while (true) {
+                // call the publishProgress method
                 // publishProgress will call the onProgressUpdate method
-                    try {
-                        Thread.sleep(1000);
-                        publishProgress(i);
-                        i++;
-                    } catch (Exception e) {
-                        Log.i("makemachine", e.getMessage());
-                    }
+                if ( isCancelled())
+                    break;
+                try {
+                    Thread.sleep(200);
+                    publishProgress(i);
+                    i++;
+                } catch (Exception e) {
+                    Log.i("makemachine", e.getMessage());
+                }
             }
             return null;
         }
 
-
-        public void stopApp(View view) {
-            //this is 'Stop' button click listener
-            switch (view.getId()) {
-                case R.id.button2:
-                    new UpdateTask().cancel(false);
-                    break;
-            }
-       }
     }
 }
---
+++ MiniProject/app/src/main/java/com/example/ipd/miniproject/MiniProject.java
@@ -0,0 +1,16 @@
+package com.example.ipd.miniproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiniProject extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mini_project);
+        
+    }
+}
